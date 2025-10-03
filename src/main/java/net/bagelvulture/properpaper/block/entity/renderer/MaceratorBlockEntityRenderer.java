91b225e97e2d69c4236df0f65c9e7f9c77721da6
BagelVulture <ethanlhuot@gmail.com>
2025-10-03 11:02:09 -0400

    added macerator's spinny things

--- src/main/java/net/bagelvulture/properpaper/block/entity/renderer/MaceratorBlockEntityRenderer.java
+++ src/main/java/net/bagelvulture/properpaper/block/entity/renderer/MaceratorBlockEntityRenderer.java
@@ -1,7 +1,7 @@
 package net.bagelvulture.properpaper.block.entity.renderer;
 
+import net.bagelvulture.properpaper.block.custom.MaceratorBlock;
 import net.bagelvulture.properpaper.block.entity.custom.MaceratorBlockEntity;
-import net.minecraft.block.Blocks;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.render.OverlayTexture;
 import net.minecraft.client.render.RenderLayer;
@@ -12,6 +12,8 @@ import net.minecraft.client.render.model.BakedModel;
 import net.minecraft.client.render.model.BakedModelManager;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.math.Direction;
+import net.minecraft.util.math.RotationAxis;
 
 public class MaceratorBlockEntityRenderer implements BlockEntityRenderer<MaceratorBlockEntity> {
     public MaceratorBlockEntityRenderer(BlockEntityRendererFactory.Context context) {}
@@ -19,19 +21,53 @@ public class MaceratorBlockEntityRenderer implements BlockEntityRenderer<Macerat
     @Override
     public void render(MaceratorBlockEntity entity, float tickDelta, MatrixStack matrices,
                        VertexConsumerProvider vertexConsumers, int light, int overlay) {
+        if (entity.getWorld() == null) return;
+
+        Direction facing = entity.getCachedState().get(MaceratorBlock.FACING);
         MinecraftClient client = MinecraftClient.getInstance();
         BakedModelManager modelManager = client.getBakedModelManager();
-
         BakedModel rollers = modelManager.getModel(Identifier.of("proper-paper", "block/macerator_spinny_things"));
 
+        float time = (entity.getWorld().getTime() + tickDelta);
+        float speed = 4.0f;
+
         matrices.push();
-
-        client.getBlockRenderManager().getModelRenderer().render(
-                entity.getWorld(), rollers, Blocks.AIR.getDefaultState(), entity.getPos(), matrices,
-                vertexConsumers.getBuffer(RenderLayer.getCutout()), false, entity.getWorld().random, 0,
-                OverlayTexture.DEFAULT_UV
-        );
-
+        switch (facing) {
+            case NORTH, SOUTH -> {
+                matrices.translate(0.5, 0.8125, 0.28125);
+                matrices.multiply(RotationAxis.POSITIVE_X.rotationDegrees((time * speed) % 360));
+            }
+            case EAST, WEST -> {
+                matrices.translate(0.28125, 0.8125, 0.5);
+                matrices.multiply(RotationAxis.POSITIVE_Y.rotationDegrees(90));
+                matrices.multiply(RotationAxis.POSITIVE_X.rotationDegrees((time * speed) % 360));
+            }
+        }
+        renderModel(client, rollers, matrices, vertexConsumers, light);
         matrices.pop();
+
+
+        matrices.push();
+        switch (facing) {
+            case NORTH, SOUTH -> {
+                matrices.translate(0.5, 0.8125, 0.71875);
+                matrices.multiply(RotationAxis.POSITIVE_X.rotationDegrees(((-time * speed) % 360) + 22.5f));
+            }
+            case EAST, WEST -> {
+                matrices.translate(0.71875, 0.8125, 0.5);
+                matrices.multiply(RotationAxis.POSITIVE_Y.rotationDegrees(90));
+                matrices.multiply(RotationAxis.POSITIVE_X.rotationDegrees(((-time * speed) % 360) + 22.5f));
+            }
+        }
+        renderModel(client, rollers, matrices, vertexConsumers, light);
+        matrices.pop();
+    }
+
+    private void renderModel(MinecraftClient client, BakedModel model, MatrixStack matrices,
+                             VertexConsumerProvider vertexConsumers, int light) {
+        client.getBlockRenderManager().getModelRenderer().render(
+                matrices.peek(), vertexConsumers.getBuffer(RenderLayer.getCutout()), null, model,
+                1.0f, 1.0f, 1.0f, light, OverlayTexture.DEFAULT_UV
+        );
     }
 }
